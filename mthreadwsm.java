139412c6421941212422ddc977f32f7c3009e5cc
programminghubb <35744285+programminghubb@users.noreply.github.com>
2018-04-03 00:37:05 +0545

    Create mthreadwsm.java

---
+++ mthreadwsm.java
@@ -0,0 +1,45 @@
+class Oddnum{  
+     //defining synchronized method
+    synchronized void printoddnum(int n){ 
+       for(int i=1;i<=n;i+=2){
+               System.out.println(i);
+           }  
+         try{  
+          Thread.sleep(500);  
+         }catch(Exception e){System.out.println(e);}  
+       }  
+      
+     }  
+    class Thread1 extends Thread{  
+        Oddnum o;
+        Thread1(Oddnum o){
+            this.o=o;
+        }
+        
+        public void run(){
+        o.printoddnum(10);
+        
+    }
+      
+    }  
+    class Thread2 extends Thread{    
+        Oddnum o;
+        Thread2(Oddnum o){
+            this.o=o;
+        }
+        
+        public void run(){
+        o.printoddnum(20); 
+    
+    }  
+    }  
+      
+    class ThreadTesting{  
+    public static void main(String args[]){  
+    Oddnum object = new Oddnum();//only one object  
+    Thread1 t1 = new Thread1(object);  
+    Thread2 t2 = new Thread2(object);  
+    t1.start();  
+    t2.start(); 
+    }  
+    }  
